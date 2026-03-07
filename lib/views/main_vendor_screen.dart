3dcddf498233df968914b64d3049a42aa99177da
图阿里 <zylyw123@qq.com>
2026-03-07 18:34:20 +0800

    底部导航栏

--- lib/views/main_vendor_screen.dart
+++ lib/views/main_vendor_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainVendorScreen extends StatefulWidget {
@@ -8,15 +9,36 @@ class MainVendorScreen extends StatefulWidget {
 }
 
 class _MainVendorScreenState extends State<MainVendorScreen> {
+  int _pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Vendor Dashboard')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [Text('Welcome to Vendor Dashboard')],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIndex,
+        onTap: (index) {
+          setState(() {
+            _pageIndex = index;
+          });
+        },
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.money_dollar),
+            label: 'Earnings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.upload_circle),
+            label: 'Upload',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.edit), label: 'Edit'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.logout), label: 'Logout'),
+        ],
       ),
     );
   }
